be24a0c78ef1d1f99d43619b475d65616d217901
thanh <thanhdx86@gmail.com>
2020-06-27 22:19:49 +0700

    Fix: cache the page's repository

--- modules/Page/Repositories/Cache/CachePageRepository.php
+++ modules/Page/Repositories/Cache/CachePageRepository.php
@@ -1,13 +1,19 @@
 <?php
 namespace Modules\Page\Repositories\Cache;
 
-use Modules\Core\Repositories\BaseRepository;
+use Modules\Core\Repositories\Cache\BaseCacheDecorator;
 use Modules\Page\Repositories\PageRepository;
 
-class CachePageRepository implements BaseRepository {
+class CachePageRepository extends BaseCacheDecorator implements PageRepository {
     protected $repository;
     public function __construct(PageRepository $pageRepository)
     {
+        parent::__construct();
         $this->repository = $pageRepository;
+    }
+
+    public function forceDestroy($page)
+    {
+        return $this->repository->forceDestroy($page);
     }
 }
